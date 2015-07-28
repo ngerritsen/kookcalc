8f27bd4faed1f93d79c96527e4822b422ae3570c
Aesthetickz <n.c.gerritsen@gmail.com>
2015-07-28 20:40:30 +0200

    changed : to ::

--- scripts/input-form.js
+++ scripts/input-form.js
@@ -5,7 +5,7 @@ riot.tag(
 	`
 	<form class="form cf">
 		<div each="{food in opts.foods.toJS()}" class="form-segment">
-			<div class="form-element form-title">{food.foodType}: </div>
+			<div class="form-element form-title">{food.foodType}:: </div>
 			<pan-selector id="{food.id}" panWeight="{food.panWeight}" class="form-element pan-selector"></pan-selector>
 			<weight-input id="{food.id}" weight="{food.weight}" class="form-element weight-input"></weight-input>
 		</div>
