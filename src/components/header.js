4431052c3bab9b7af087794f98d06f8ace5a64d7
Niels Gerritsen <n.c.gerritsen@gmail.com>
2016-11-18 22:02:46 +0100

    Improve styling

--- src/components/header.js
+++ src/components/header.js
@@ -3,6 +3,8 @@ import '../styles/header.scss';
 
 export default function Header() {
   return <header className="header">
-    <h1 className="header__title">Kookcalc</h1>
+    <h1 className="header__title">
+      <strong>Kook</strong>calc
+    </h1>
   </header>
 }
